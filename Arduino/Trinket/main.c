2eb6955bd5f973d430d2638820d40d3102465c91
Léon van Velzen <leon-vv@hotmail.com>
2015-11-07 14:43:51 +0100

    Add arming procedure

--- Arduino/Trinket/main.c
+++ Arduino/Trinket/main.c
@@ -1,59 +1,63 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
-#include <avr/delay.h>
 #include <util/delay_basic.h>
 #include <stdint.h>
 
-#define SERVO_SHORT_PULSE 500
+#define SERVO_SHORT_PULSE 530
 #define SERVO_LONG_PULSE 2400
-#define SERVO_SHORT_CYCLES (8 * SERVO_SHORT_PULSE)
-#define SERVO_LONG_CYCLES (8 * SERVO_LONG_PULSE)
 #define SERVO_PIN PB2
 
-#define ESC_SHORT_PULSE 1000
-#define ESC_LONG_PULSE 2000
-#define ESC_SHORT_CYCLES (8 * ESC_SHORT_PULSE)
-#define ESC_LONG_CYCLES (8 * ESC_LONG_PULSE)
-#define ESC_PIN PB1
+#define ESC_SHORT_PULSE 650
+#define ESC_LONG_PULSE 1200
+#define ESC_PIN PB4
 
 // An 8-bit value representing the state of the servo.
 // If this value is 0 the servo shaft will be turned
 // all the way to one side, if this value is 255
 // the shaft will be turned the other way around.
 volatile uint8_t servo = 200;
-volatile uint8_t esc = 100;
+volatile uint8_t esc = 200;
 
 void inline
-set_on_for(unsigned iterations, uint8_t pin)
+delay_microseconds(unsigned us)
 {
-    PORTB |= _BV(pin);
-    _delay_loop_2(iterations);
-    PORTB &= ~_BV(pin);
+    _delay_loop_2(2 * us);
 }
 
 void inline
-pulse_for(unsigned pulse_duration, int duration, uint8_t pin)
+delay_milliseconds(unsigned ms)
 {
-    while(duration > 0) {
-        _delay_ms(18);
-        duration -= 18;
+    delay_microseconds(ms * 1000);
+}
+    
+void inline
+pulse_for(unsigned pulse_duration_us, int duration_ms, uint8_t pin)
+{
+    while(duration_ms > 0) {
+        delay_milliseconds(18);
+        duration_ms -= 18;
 
-        set_on_for(pulse_duration * 2, pin);
+        PORTB |= _BV(pin);
+        delay_microseconds(pulse_duration_us);
+        PORTB &= ~_BV(pin);
     }
 }
 
 void
-calibrate_esc() {
-    pulse_for(1000, 5000, ESC_PIN);
-    pulse_for(1500, 5000, ESC_PIN);
-    pulse_for(2000, 5000, ESC_PIN);
+arm_esc() {
+    // The ESC will only turn the motor on
+    // if it receives short pulses at the start.
+    // I think this is a security measure.
+    pulse_for(ESC_SHORT_PULSE, 4000, ESC_PIN);
 }
 
 void inline
 sort_delay_array(uint32_t *array) {
-    // Sort the array by the amount of iterations to delay (ascending).
-
+    // Sort the array by the amount of microseconds to delay (ascending).
+    // The amount of microseconds to delay is stored in the 16
+    // least significant bits.
+    
     uint16_t first = array[0];
     uint16_t second = array[1];
 
@@ -75,20 +79,14 @@ ISR(TIM0_COMPA_vect) {
     // SERVO_LONG_CYCLES = (SERVO_LONG_PULSE / 10^6) * 8 * 10^6 = 8 * SERVO_LONG_PULSE and
     // SERVO_SHORT_CYCLES = (SERVO_SHORT_PULSE / 10^6) * 8 * 10^6 = 8 * SERVO_SHORT_PULSE cpu cycles.
     
-    // So we first translate 'servo', which is a value between 0 and 255
-    // to the necessary cycles.
-    uint16_t servo_cycles_to_wait = SERVO_SHORT_CYCLES + (servo / 255.0) * (SERVO_LONG_CYCLES - SERVO_SHORT_CYCLES);
-    uint16_t esc_cycles_to_wait = ESC_SHORT_CYCLES + (esc / 255.0) * (ESC_LONG_CYCLES - ESC_SHORT_CYCLES);
-
-    // '_delay_loop_2' delayes the program by a number of iterations.
-    // Every iteration takes four cycles, as documented on the following
-    // page of the AVR Libc reference:nongnu.org/avr-libc/user-manual/group__util__delay__basic.html
-    uint16_t servo_iter_to_wait = servo_cycles_to_wait / 4;
-    uint16_t esc_iter_to_wait = esc_cycles_to_wait / 4;
+    // We first translate the control variables 'esc' and 'servo'
+    // whose values are between 0 adn 255 to the amount of microseconds to wait.
+    uint16_t servo_us_delay = SERVO_LONG_PULSE * (servo/255.0);
+    uint16_t esc_us_delay = ESC_LONG_PULSE * (esc/255.0);
 
     uint32_t delays[2] = {
-        ((uint32_t)SERVO_PIN << 16) + servo_iter_to_wait,
-        ((uint32_t)ESC_PIN << 16) + esc_iter_to_wait
+        ((uint32_t)SERVO_PIN << 16) | servo_us_delay,
+        ((uint32_t)ESC_PIN << 16) | esc_us_delay
     };
 
     sort_delay_array(delays);
@@ -97,12 +95,14 @@ ISR(TIM0_COMPA_vect) {
     PORTB |= _BV(SERVO_PIN) | _BV(ESC_PIN);
 
     uint16_t delayed = 0;
+
     for(int i = 0; i < 2; i++) {
         uint32_t current = delays[i];
         
         unsigned delay = ((uint16_t)current) - delayed;
 
-        set_on_for(delay, current >> 16); 
+        delay_microseconds(delay);
+        PORTB &= ~_BV(current >> 16);
 
         delayed += delay;
     }
@@ -112,7 +112,7 @@ int main() {
     // Set the pins as output.
     DDRB |= _BV(SERVO_PIN) | _BV(ESC_PIN);
 
-    calibrate_esc(); 
+    arm_esc(); 
     
     // Enable interrupts.
     sei();
